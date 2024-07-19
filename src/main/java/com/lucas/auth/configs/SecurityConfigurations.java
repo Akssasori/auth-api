59b27709646e5892cc57487732f6d33adc203418
Lucas <diniz3003@gmail.com>
2024-07-19 12:38:55 -0300

    Blocked requests, all will require a token and /product only admin

--- src/main/java/com/lucas/auth/configs/SecurityConfigurations.java
+++ src/main/java/com/lucas/auth/configs/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.lucas.auth.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,11 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // para requisição post no /product precisa ser admin para a outras só preciso que ele seja autenticado
+                // ou seja agora tudo vai estar fechado ate que a gente autentique
+                .authorizeHttpRequests( authorize -> authorize.requestMatchers(HttpMethod.POST,"/product").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
