4a72c5997ebd6574f0026639dd8672de899a3a4f
Lucas <diniz3003@gmail.com>
2024-07-20 10:14:07 -0300

    add library auth0

---
+++ src/main/java/com/lucas/auth/services/TokenService.java
@@ -0,0 +1,53 @@
+package com.lucas.auth.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.lucas.auth.models.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algoritm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algoritm);
+        }catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating token", exception);
+        }
+
+    }
+
+    public String verifyToken(String token) {
+        try {
+            Algorithm algoritm = Algorithm.HMAC256(secret);
+            return JWT.require(algoritm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        }catch (JWTVerificationException exception) {
+            return "";
+
+        }
+    }
+
+    private Instant genExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
